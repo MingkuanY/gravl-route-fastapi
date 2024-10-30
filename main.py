4bc92ec0e3b73d685929a00ed45ee844dfd95766
Mingkuan Yan <86630468+MingkuanY@users.noreply.github.com>
2024-10-30 12:18:53 -0400

    Updated cors to accept subdomain

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["http://localhost:3000", "https://www.gravl.org"],
+  allow_origins=["http://localhost:3000", "https://www.gravl.org", "https://api.gravl.org"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
